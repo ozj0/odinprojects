3c8acb86371f9a5739c989099dfd20153f1b8dbd
ozlure <ozlur3@gmail.com>
2022-11-27 11:11:52 -0800

    Create script.js, write some pseudocode

---
+++ odin-rps/script.js
@@ -0,0 +1,23 @@
+// prompt USER to type ROCK,PAPER,SCISSORS
+// store response in a variable
+// create function to make response uppercase
+// if USER response is not equal to ROCK,PAPER,SCISSORS
+//      prompt "Incorrect response, please type Rock, Paper, or Scissors"
+// if USER response is equal to ROCK,PAPER,SCISSORS
+//      assign playerChoice variable 
+// create randomNum() function to get a random number 0-2
+// create getCpuChoice() function, which uses randomNum(), to get 
+// the computer's choice. 0 = ROCK, and so on.
+// store the result from getCpuChoice() in computerChoice
+// create game() function 
+//      switch statement for game logic and different outcomes
+// 
+
+
+
+// declare the choice functions globally, then use them in the game function. 
+// assign the variables in the function ?
+
+function randomNum() {
+    return Math.floor((Math.random) * 3);
+}
