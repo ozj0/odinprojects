8b6ba14ae3c8c007c7370946f15c687eaaba125b
ozlure <ozlur3@gmail.com>
2022-11-27 20:43:43 -0800

    Create functions to get player & CPU choices

--- odin-rps/script.js
+++ odin-rps/script.js
@@ -10,8 +10,7 @@
 // the computer's choice. 0 = ROCK, and so on.
 // store the result from getCpuChoice() in computerChoice
 // create game() function 
-//      switch statement for game logic and different outcomes
-// 
+//      switch statement for game logic and different outcome
 
 
 
@@ -19,5 +18,34 @@
 // assign the variables in the function ?
 
 function randomNum() {
-    return Math.floor((Math.random) * 3);
+    return Math.floor(Math.random()* 3);
+}
+function getCpuChoice() {
+    switch(randomNum()) {
+        case 0:
+            return 'ROCK';
+        break;
+        case 1:
+            return 'PAPER';
+        break;
+        case 2:
+            return 'SCISSORS';
+    }
+}
+function getPlrChoice() {
+    let response = prompt('Please choose rock, paper, or scissors').toUpperCase();
+    if (response === 'ROCK' || response === 'PAPER' || response === 'SCISSORS') {   
+        return response;
+    } else {
+        getPlrChoice();
+    }
+}
+
+function game() {
+    let playerChoice = x;
+    let computerChoice = getCpuChoice();
+        if (playerChoice === 'ROCK' && computerChoice === 'SCISSORS') {}
+
+
+
 }
