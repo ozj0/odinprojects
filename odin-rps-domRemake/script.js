033e089a27c689b2559bd190144e249591eed87c
ozlure <ozlur3@gmail.com>
2022-12-08 20:08:09 -0600

    remake it all for fun

--- odin-rps-domRemake/script.js
+++ odin-rps-domRemake/script.js
@@ -1,54 +1,123 @@
+const btns = document.querySelectorAll('button');
+const scoreboard = document.getElementById('scoreboard');
+const results = document.getElementById('results');
 
-function random() {
+let pS = 0;
+let cS = 0;
+
+function r() {
     return Math.floor(Math.random() * 3);
 }
-function getCpuChoice() {
-    switch (random()) {
+
+function gc() {
+    switch (r()){
         case 0:
-            return 'ROCK';
+            return 'rock';
         break;
         case 1:
-            return 'PAPER';
+            return 'paper';
         break;
         case 2:
-            return 'SCISSORS';
+            return 'scissors';
+        break;
     }
 }
-const btn = document.querySelectorAll('.gameBtn');
-let playerScore = 0;
-let cpuScore = 0;
-btn.forEach((button) => {
-    button.addEventListener('click', (e) => {
-        let plrChoice = e.target.value;
-        let cpuChoice = getCpuChoice();
-        document.querySelector('.plrScore').textContent = playerScore; 
-        document.querySelector('.cpuScore').textContent = cpuScore;
-        document.querySelector('.plrScore2').textContent = playerScore;
-        document.querySelector('.cpuScore2').textContent = cpuScore;
-        if ((plrChoice === 'ROCK' && cpuChoice === 'SCISSORS') ||
-        (plrChoice === 'PAPER' && cpuChoice === 'ROCK') ||
-        (plrChoice === 'SCISSORS' && cpuChoice === 'PAPER')) {
-            playerScore++;
-        }
-        else if  ((cpuChoice === 'ROCK' && plrChoice === 'SCISSORS') ||
-        (cpuChoice === 'PAPER' && plrChoice === 'ROCK') ||
-        (cpuChoice === 'SCISSORS' && plrChoice === 'PAPER')) {
-            cpuScore++;
-        } 
-        else {
-            return;
-        }
-        if (playerScore>4) {
-            alert(`you win ${playerScore} - ${cpuScore} \n
-            click any button to play again`);
-            playerScore = 0;
-            cpuScore = 0;
-        }
-        if (cpuScore >4) {
-            alert(`you lose${playerScore} - ${cpuScore} \n
-            click any button to play again;`);
-            playerScore = 0;
-            cpuScore=0;
-        }
+
+function g(x,y) {
+    if ((x === 'rock' && y === 'scissors') ||
+        (x === 'paper' && y === 'rock') ||
+        (x === 'scissors' && y === 'paper')) return 'p';
+    if ((y === 'rock' && x === 'scissors') ||
+        (y === 'paper' && x === 'rock') ||
+        (y === 'scissors' && x === 'paper')) return 'c';
+    else return 'tie';
+}
+
+function c(w) {
+    let f = w.toString().toUpperCase().charAt(0);
+    let r = w.toString().slice(1);
+    return f + r;
+}
+
+function uS() {
+    const b1 = document.getElementById('b1');
+        b1.textContent = `${pS}`
+    const b2 = document.getElementById('b2');
+        b2.textContent = `${pS}`
+    const r1 = document.getElementById('r1');
+        r1.textContent = `${cS}`
+    const r2 = document.getElementById('r2');
+        r2.textContent = `${cS}`
+}
+
+function rsG() {
+    if (pS > 4) {
+        results.textContent = '';
+        const div = document.createElement('div');
+        div.innerHTML = `<span class="blue">Player</span> Wins!`;
+        const div2 = document.createElement('div');
+        div2.innerHTML = `Final Score: <span class="blue">5<span> - <span class="red">${cS}<span>`;
+        results.appendChild(div);
+        results.appendChild(div2);        
+        pS = 0;
+        cS = 0;
     }
-)});
+    if (cS > 4) {
+        results.textContent = '';
+        const div = document.createElement('div');
+        div.innerHTML = `<span class="red">Computer</span> Wins!`;
+        const div2 = document.createElement('div');
+        div2.innerHTML = `Final Score: <span class="red">5<span> - <span class="blue">${pS}<span>`;
+        results.appendChild(div);
+        results.appendChild(div2);        
+        pS = 0;
+        cS = 0;
+    }
+}
+btns.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        let pC = e.target.value;
+        let cC = gc();
+        results.textContent = ''
+        if (g(pC,cC) === 'p') {
+            pS++;
+            results.textContent = '';
+            const div = document.createElement('div');
+            div.textContent = `Player won that round!`
+            const div2 = document.createElement('div');
+            div2.innerHTML = `<span class="blue">Player</span> Move: ${c(pC)}`;
+            const div3 = document.createElement('div');
+            div3.innerHTML = `<span class="red">Computer</span> Move: ${c(cC)}`;
+            results.appendChild(div);
+            results.appendChild(div2);
+            results.appendChild(div3);
+        }
+        if (g(pC,cC) === 'c') {
+            cS++;
+            results.textContent = '';
+            const div = document.createElement('div');
+            div.textContent = `Computer won that round!`
+            const div2 = document.createElement('div');
+            div2.innerHTML = `<span class="blue">Player</span> Move: ${c(pC)}`;
+            const div3 = document.createElement('div');
+            div3.innerHTML = `<span class="red">Computer</span> Move: ${c(cC)}`;
+            results.appendChild(div);
+            results.appendChild(div2);
+            results.appendChild(div3);
+        }
+        if (g(pC,cC) === 'tie') {
+            results.textContent = '';
+            const div = document.createElement('div');
+            div.textContent = `It was a tie!`
+            const div2 = document.createElement('div');
+            div2.innerHTML = `<span class="blue">Player</span> Move: ${c(pC)}`;
+            const div3 = document.createElement('div');
+            div3.innerHTML = `<span class="red">Computer</span> Move: ${c(cC)}`;
+            results.appendChild(div);
+            results.appendChild(div2);
+            results.appendChild(div3);
+        }
+        uS();
+        rsG();
+    });
+})
